6ca182d9bc30fa00199b2b1ae34629d67bff1eb7
LAPTOP-R60VRCK7\joann <joannakaletaa@gmail.com>
2020-06-18 15:36:40 +0200

    fixed buttons for mongo login / registration

--- MarketPlace/api/marketplace/routes.py
+++ MarketPlace/api/marketplace/routes.py
@@ -150,7 +150,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login_mongo'))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/register_mongo", methods=['GET', 'POST'])
@@ -173,7 +173,7 @@ def register_mongo():
                 'image_file':'default.jpg'}
         m_db.users.insert_one(user)
         flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('login_mongo'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
